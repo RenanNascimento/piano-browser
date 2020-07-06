ff00b29348e5e0bcdb0f0286aaa7661606440694
renan <renan.nascimento@sympla.com.br>
2020-07-05 22:15:41 -0300

    add: piano notes

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+document.addEventListener("keydown", getKey);
+
+function addElement(note) {
+  const newDiv = document.createElement("div");
+  newDiv.id = "block";
+  newDiv.className = "block";
+  const currentDiv = document.getElementById(note);
+  currentDiv.appendChild(newDiv, currentDiv);
+}
+
+function removeAnimation(note) {
+  const newDiv = document.createElement("div");
+  newDiv.id = "block-remove";
+  newDiv.className = "block-remove";
+  const currentDiv = document.getElementById(note);
+  currentDiv.appendChild(newDiv, currentDiv);
+}
+
+function getKey(e) {
+  if (e.code == "KeyC") {
+    addElement();
+  }
+  if (e.code == "KeyX") {
+    removeAnimation();
+  }
+}
+
 WebMidi.enable((err) => {
   if (err) {
     console.log("WebMidi could not be enabled.", err);
@@ -8,15 +35,17 @@ WebMidi.enable((err) => {
   console.log(WebMidi.outputs);
 
   // Retrieve an input by name
-  const input = WebMidi.getInputByName("DigitalKBD MIDI 1");
+  const input = WebMidi.getInputByName("Digital Piano MIDI 1");
 
   // Listen for a 'note on' message on all channels
   input.addListener("noteon", "all", (e) => {
     console.log(`Received 'noteon' message (${e.note.name}${e.note.octave})`);
+    addElement(`${e.note.name}${e.note.octave}`);
   });
 
   // Listen for a 'note off' message on all channels
   input.addListener("noteoff", "all", (e) => {
     console.log(`Received 'noteoff' message (${e.note.name}${e.note.octave})`);
+    removeAnimation(`${e.note.name}${e.note.octave}`);
   });
 });
